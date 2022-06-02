8caa28c8dce059fa17723b597543790d95c11338
GregGregK <gregori_monteiro@estudante.sc.senai.br>
2022-06-02 20:24:06 -0300

    Greg - Ibridge

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  margin: 30,
+  autoplay: true,
+  autoplayTimeout: 6000,
+  dots: true,
+  lazyLoad: true,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
